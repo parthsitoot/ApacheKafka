29b56ecec9eb0ca3bfa102069a893099ca37f8ff
Parth Sitoot <parth.sitoot@gmail.com>
2025-12-10 22:28:54 +0530

    Adding spring project

---
+++ spring-kafka-integration/src/main/java/net/apachekafka/spring_kafka_integration/SpringKafkaIntegrationApplication.java
@@ -0,0 +1,13 @@
+package net.apachekafka.spring_kafka_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaIntegrationApplication.class, args);
+	}
+
+}
---
+++ spring-kafka-integration/src/test/java/net/apachekafka/spring_kafka_integration/SpringKafkaIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.apachekafka.spring_kafka_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
